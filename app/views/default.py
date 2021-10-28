b08b883867e2fd7780653173f64e7b8bfbf43eee
rbneiman <alecneiman@gmail.com>
2021-10-28 17:05:12 -0500

    Initial commit

--- app/views/default.py
+++ app/views/default.py
@@ -6,3 +6,27 @@ bp = Blueprint('test', __name__, url_prefix='/')
 @bp.route("/")
 def get_site():
     return render_template("site.html")
+
+
+@bp.route("/datasets")
+def get_datasets():
+    """ Get the valid datasets stored on this server.
+    :return: A json file containing info for all valid datasets stored on this server.
+    """
+    # TODO
+    return "{}"
+
+
+"""
+
+react client calls <flask server>/datasetList
+flask server gives list of datasets with info including names, urls, etc...
+react client makes fancy list from that
+user clicks button
+react client calls <flask server>/datasets/<selected file>
+<flask server> gives response, and response is downloaded to client computer
+
+http://192.168.18.245:8080/datasets/abdominal-and-direct-fetal-ecg-database-1.0.0.zip
+
+
+"""
