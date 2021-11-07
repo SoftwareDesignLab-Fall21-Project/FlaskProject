907a801a896a975592d5f3996eeac5dbd2b1e68d
tbert93 <tony.bertolino@utexas.edu>
2021-11-07 15:12:03 -0600

    Added route /hwcapacity

--- server.py
+++ server.py
@@ -4,6 +4,16 @@ import os
 
 application = app_factory.create_app()
 
+#returns the capacity of the hardware sets
+#using placeholder until DB is setup
+@application.route('/hwcapacity')
+def getHwCapacity():
+    return{
+        "Set 1": "5",
+    }
+
+
+
 if __name__ == '__main__':
     run_simple(
         '0.0.0.0', 8080,
