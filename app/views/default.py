95b4fe4a26503f495daf1e8611943ecd56a02d12
MandrewWhit <andrew.whitaker987@gmail.com>
2021-11-08 15:22:33 -0600

    database initialized

--- app/views/default.py
+++ app/views/default.py
@@ -1,20 +1,80 @@
-from flask import Blueprint, render_template
+from flask import Flask, Blueprint, render_template, Response, jsonify
 from flask_cors import CORS, cross_origin
+from bson.objectid import ObjectId
+from database import mongo
 
 bp = Blueprint('test', __name__, url_prefix='/')
 
 
-@bp.route('/hwcapacity')
+
+@bp.route('/hwcapacity', methods=["GET"])
 @cross_origin()
 def getHwCapacity():
     """
     using placeholder until DB is setup
     :return: the capacity of the hardware sets
     """
+    col = mongo.db.HardwareSets
+    col.insert_one({
+        "HardwareSet": "3",
+        "Capacity": "2",
+        "Availability": "3"
+    })
+    print(col.find_one())
     return {
         "Set 1": "5",
     }
 
+
+@bp.route('/set-capacity/<id>', methods=["PATCH"])
+def set_capacity(id):
+    print(set_capacity)
+    try:
+        dbResponse = mongo.db.HardwareSets.update_one(
+            {"_id":ObjectId(id)},
+            {"$set":{"Capacity":request.form["Capacity"]}}
+        )
+        for attr in dir(dbResponse):
+            print(f"{attr}")
+        return Response(
+            response = jsonify({
+                "message":"capacity updated"
+            }),
+            status = 200,
+        )
+    except Exception as ex:
+        print(ex)
+        return Response(
+            response = jsonify({
+                "message":"unable to update capacity"
+            }),
+            status = 500,
+        )
+
+@bp.route('/set-available/<id>', methods=["PATCH"])
+def set_available(id):
+    print(set_available)
+    try:
+        dbResponse = mongo.db.HardwareSets.update_one(
+            {"_id":ObjectId(id)},
+            {"$set":{"Available":request.form["Available"]}}
+        )
+        for attr in dir(dbResponse):
+            print(f"{attr}")
+        return Response(
+            response = jsonify({
+                "message":"availability updated"
+            }),
+            status = 200,
+        )
+    except Exception as ex:
+        print(ex)
+        return Response(
+            response = jsonify({
+                "message":"unable to update availability"
+            }),
+            status = 500,
+        )
 
 @bp.route("/")
 def get_site():
--- server.py
+++ server.py
@@ -2,14 +2,22 @@ from app import app_factory
 from werkzeug.serving import run_simple
 from flask_cors import CORS
 import os
+from database import mongo
 
 application = app_factory.create_app()
 cors = CORS(application)
 application.config['CORS_HEADERS'] = 'Content-Type'
 
+try:
+    application.config['MONGO_URI'] = "mongodb+srv://tbertolino:softwarelabfall2021@cluster0.mphmj.mongodb.net/SoftwareDesignLab-Fall21-Project?retryWrites=true&w=majority"
+    mongo.init_app(application)
+except Exception as ex:
+    print("*****ERRROR DATABASE*****")
+    print(ex)
+
 if __name__ == '__main__':
     run_simple(
-        '0.0.0.0', 8080,
+        '0.0.0.0', 9999,
         application,
         # static_files=get_static_files(),
         use_reloader=True, use_debugger=True, threaded=True
